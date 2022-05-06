8080b339f6dc52b9aaef075f87fb2ab40e4a6643
Domrev Igor <domrevigor@gmail.com>
2022-05-06 11:20:05 +0300

    alwas take custom apps if not uninstalled when getting active stroes

--- chapter0/hello.sh
+++ chapter0/hello.sh
@@ -2,3 +2,6 @@ go build hello.go
 go clean hello.go
 go build
 go run chapter0 # delete go.mod and run this
+
+go vet # idea covers you
+go format # idea covers you
---
+++ chapter4/slice_test.go
@@ -0,0 +1,17 @@
+package chapter4
+
+import (
+	"testing"
+)
+
+func TestSliceMe(t *testing.T) {
+	Slices()
+}
+
+func TestPtrSlice(t *testing.T) {
+	PointersSlice()
+}
+
+func TestStringSlice(t *testing.T) {
+	StringSlice()
+}

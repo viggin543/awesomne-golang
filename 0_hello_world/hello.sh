520259684a0e9841316609a3cd92cf525a79a7ad
Domrev Igor <domrevigor@gmail.com>
2022-05-08 22:32:21 +0300

    monitoring fast sync duration

--- 0_hello_world/hello.sh
+++ 0_hello_world/hello.sh
@@ -1,9 +1,19 @@
 go build hello.go
 go clean hello.go
 go build
-https://go.dev/ref/mod#go-install
-go install github.com/viggin543/awesomne-golang/0_hello_world@latest
-go run chapter0 # delete go.mod and run this
+go run 0_hello_world # delete go.mod and run this
 
 go vet # idea covers you
 go format # idea covers you
+
+https://go.dev/ref/mod#go-install
+go install github.com/viggin543/awesomne-golang/0_hello_world@latest
+go env | grep -i path
+# > GOPATH="/Users/domrevigor/go"
+file $GOPATH/bin/0_hello_world
+
+
+# clone, compile, execute, delete binaries
+go run github.com/viggin543/awesomne-golang/0_hello_world@latest
+
+###################################### releasing binaries and libraries was never so easy !!   ##################################

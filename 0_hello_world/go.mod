611913fe0b3f88e641d26e2733619afca643b70d
Domrev Igor <domrevigor@gmail.com>
2022-05-08 22:23:22 +0300

    monitoring fast sync duration

--- 0_hello_world/go.mod
+++ 0_hello_world/go.mod
@@ -1,3 +1,3 @@
-module chapter0
+module github.com/viggin543/awesomne-golang/chapter0
 
 go 1.18
--- 0_hello_world/hello.sh
+++ 0_hello_world/hello.sh
@@ -1,6 +1,8 @@
 go build hello.go
 go clean hello.go
 go build
+https://go.dev/ref/mod#go-install
+go install github.com/viggin543/awesomne-golang/chapter0
 go run chapter0 # delete go.mod and run this
 
 go vet # idea covers you

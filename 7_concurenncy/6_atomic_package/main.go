d7fe5488de4cd0b97407ff53eddeda80261e36b1
Domrev Igor <domrevigor@gmail.com>
2022-05-21 23:07:49 +0300

    .

--- 7_concurenncy/6_atomic_package/main.go
+++ 7_concurenncy/6_atomic_package/main.go
@@ -11,19 +11,14 @@ import (
 )
 
 var (
-	// shutdown is a flag to alert running goroutines to shutdown.
+	// shutdown is a flag to alert running goroutines to shut down.
 	shutdown int64
-
-	// wg is used to wait for the program to finish.
-	wg sync.WaitGroup
+	wg       sync.WaitGroup
 )
 
-// main is the entry point for all Go programs.
 func main() {
-	// Add a count of two, one for each goroutine.
 	wg.Add(2)
 
-	// Create two goroutines.
 	go doWork("A")
 	go doWork("B")
 
@@ -34,7 +29,6 @@ func main() {
 	fmt.Println("Shutdown Now")
 	atomic.StoreInt64(&shutdown, 1)
 
-	// Wait for the goroutines to finish.
 	wg.Wait()
 }
 
@@ -44,7 +38,7 @@ func doWork(name string) {
 	// Schedule the call to Done to tell main we are done.
 	defer wg.Done()
 
-	for {
+	for { // who needs while(true) { ...
 		fmt.Printf("Doing %s Work\n", name)
 		time.Sleep(250 * time.Millisecond)
 
---
+++ 7_concurenncy/6_mutex/main.go
@@ -0,0 +1,45 @@
+// This sample program demonstrates how to use a mutex
+// to define critical sections of code that need synchronous
+// access.
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var (
+	counter int
+
+	wg    sync.WaitGroup
+	mutex sync.Mutex
+)
+
+func main() {
+	wg.Add(2)
+
+	go incCounter()
+	go incCounter()
+
+	wg.Wait()
+	fmt.Printf("Final Counter: %d\n", counter)
+}
+
+func incCounter() {
+	defer wg.Done()
+
+	for count := 0; count < 2; count++ {
+		mutex.Lock() // this is much slower than using atomic package
+		{ // parenthesis just to emphasize below block, no functional cause to add them here.
+			value := counter
+			// Yield the thread and be placed back in queue.
+			runtime.Gosched()
+			value++
+			counter = value
+		}
+		mutex.Unlock()
+		// Release the lock and allow any
+		// waiting goroutine through.
+	}
+}

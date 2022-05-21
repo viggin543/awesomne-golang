7cf364cc43367c7aec59881e7d349b2688de4e60
Domrev Igor <domrevigor@gmail.com>
2022-05-21 23:03:00 +0300

    .

---
+++ 7_concurenncy/5_atomic_actions/main.go
@@ -0,0 +1,38 @@
+// This sample program demonstrates how to use the atomic
+// package to provide safe access to numeric types.
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
+
+var (
+	counter int64
+	wg      sync.WaitGroup
+)
+
+func main() {
+	wg.Add(2)
+
+	go incCounter()
+	go incCounter()
+
+	wg.Wait()
+
+	fmt.Println("Final Counter:", counter)
+}
+
+func incCounter() {
+	defer wg.Done()
+
+	for count := 0; count < 2; count++ {
+		// Atomic increment
+		// why Atomic increment and not just using another mutex (sync.WaitGroup) ?
+		atomic.AddInt64(&counter, 1)
+		// Yield the thread and be placed back in queue.
+		runtime.Gosched()
+	}
+}

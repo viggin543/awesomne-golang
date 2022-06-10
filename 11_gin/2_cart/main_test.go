2be820613d6deded9a870868691bd1e4dbe7595d
Domrev Igor <domrevigor@gmail.com>
2022-06-10 15:22:40 +0300

    simple cart service with connection to mongodb

--- 11_gin/2_cart/main_test.go
+++ 11_gin/2_cart/main_test.go
@@ -23,15 +23,15 @@ func TestMain(m *testing.M) {
 	cfg := models.ParseConfig()
 	cartModule = module.Create(cfg)
 	server.SetupRoutes(engine, cartModule)
-	os.Exit(m.Run())
-}
-
-func TestSaveCart(t *testing.T) {
 	//make sure mongodb is running locally
 	//	docker run -d -p27017:27017 --name mongodb \
 	//    -e MONGODB_ROOT_USER=root -e MONGODB_ROOT_PASSWORD=root \
 	//    bitnami/mongodb:latest
-	cartModule.MongoDataSource.Drop(ctx) // cleanup b4 test
+	os.Exit(m.Run())
+}
+
+func TestSaveCart(t *testing.T) {
+	clenaup(t)
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("PUT", "/cart", strings.NewReader(`
 {
@@ -39,12 +39,20 @@ func TestSaveCart(t *testing.T) {
   "totalCents": 100
 }`))
 	req.Header.Add("Content-Type", "application/json")
-	defer cartModule.MongoDataSource.Drop(ctx) // avoid sharing state between tests, tests need to be independent !
+	defer clenaup(t) // avoid sharing state between tests, tests need to be independent !
 
 	engine.ServeHTTP(w, req)
 
 	assert.EqualValues(t, http.StatusOK, w.Code)
-	documents, err := cartModule.MongoDataSource.Collection("cart").CountDocuments(ctx, bson.M{})
+	one := cartModule.MongoDataSource.Collection("cart").FindOne(ctx, bson.M{"id": "iddqd"})
+	assert.Nil(t, one.Err())
+	cart := models.Cart{}
+	err := one.Decode(&cart) // this works because of the bson struct tags
 	assert.Nilf(t, err, "err %v", err)
-	assert.EqualValues(t, 1, documents)
+	assert.EqualValues(t, cart.TotalCents, 100)
+}
+
+func clenaup(t *testing.T) {
+	err := cartModule.MongoDataSource.Drop(ctx)
+	assert.Nilf(t, err, "err %v", err)
 }
